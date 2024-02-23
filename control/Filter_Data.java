54cf6c38ce8c17ff14212a04ed23de77814b3057
Caine Bautista <caineivan.bautista@g.msuiit.edu.ph>
2024-02-23 21:51:15 +0800

    Fix filter so that it filters the exact input, not just the pattern

--- control/Filter_Data.java
+++ control/Filter_Data.java
@@ -2,6 +2,7 @@ package control;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.swing.JTable;
 import javax.swing.RowFilter;
@@ -31,7 +32,11 @@ public class Filter_Data {
         table.setRowSorter(row_sorter);
         table.getRowSorter().toggleSortOrder(0); // to still sort the filtered data based on the first column
 
-        row_sorter.setRowFilter(RowFilter.regexFilter(input_search, column_index));
+        // to filter the exact pattern
+        String text_regex = Pattern.quote(input_search);
+        String search_text = String.format("^%s$", text_regex);
+
+        row_sorter.setRowFilter(RowFilter.regexFilter(search_text, column_index));
     }
 
     /**
@@ -48,17 +53,19 @@ public class Filter_Data {
 
         List<RowFilter<Object, Object>> filters = new ArrayList<RowFilter<Object, Object>>(columns.length);
 
+        // filter the exact pattern
         for (int count = 0; count < columns.length; count++)
-            filters.add(RowFilter.regexFilter(columns[count], indices[count]));
+            filters.add(RowFilter.regexFilter(String.format("^%s$", Pattern.quote(columns[count])), indices[count]));
 
         row_sorter.setRowFilter(RowFilter.andFilter(filters));
     }
 
     /**
      * Cancel the filter.
+     * 
      * @param table
      */
-    public static void cancelFilter(JTable table){
+    public static void cancelFilter(JTable table) {
         def_model = (DefaultTableModel) table.getModel();
         row_sorter = new TableRowSorter<DefaultTableModel>(def_model);
         table.setRowSorter(row_sorter);
